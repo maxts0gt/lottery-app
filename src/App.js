700601efcf05ea122f6a5c7195613af847edc700
maxts0gt <bazarsadtsogtjargal@gmail.com>
2021-10-21 15:45:40 +0900

    create lottery app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Lottery from './Lottery.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Lottery />
+			<Lottery title='Mini Daily' maxNum={10} maxBalls={4} />
+		</div>
+	);
 }
 
 export default App;
